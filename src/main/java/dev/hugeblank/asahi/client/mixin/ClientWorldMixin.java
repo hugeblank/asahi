9c1e216907d4766914f294975e0d35aee78d5066
hugeblank <hugeblank@gmail.com>
2025-02-07 00:36:58 -0800

    fix: Make it actually work

--- src/main/java/dev/hugeblank/asahi/client/mixin/ClientWorldMixin.java
+++ src/main/java/dev/hugeblank/asahi/client/mixin/ClientWorldMixin.java
@@ -2,11 +2,10 @@ package dev.hugeblank.asahi.client.mixin;
 
 import dev.hugeblank.asahi.client.EvictingList;
 import dev.hugeblank.asahi.client.TimeSmoother;
-import net.minecraft.client.world.ClientWorld;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.network.packet.WorldTimeUpdateS2CPacket;
 import net.minecraft.util.profiler.Profiler;
 import net.minecraft.world.ExtendedBlockView;
-import net.minecraft.world.GameRules;
 import net.minecraft.world.IWorld;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.ChunkManager;
@@ -14,6 +13,9 @@ import net.minecraft.world.dimension.Dimension;
 import net.minecraft.world.dimension.DimensionType;
 import net.minecraft.world.level.LevelProperties;
 import org.spongepowered.asm.mixin.*;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.function.BiFunction;
 
@@ -23,6 +25,8 @@ public abstract class ClientWorldMixin implements ExtendedBlockView, IWorld, Aut
     @Unique private final EvictingList<Double> points = new EvictingList<>(10);
     @Unique private double factor = 0D;
     @Unique private double remainder = 0D;
+    @Unique private long lastPacketTimeOfDay = 0;
+    @Unique private boolean shouldTickDay = true;
 
     protected ClientWorldMixin(
             LevelProperties levelProperties, DimensionType dimensionType, BiFunction<World, Dimension, ChunkManager> biFunction, Profiler profiler, boolean bl
@@ -30,21 +34,17 @@ public abstract class ClientWorldMixin implements ExtendedBlockView, IWorld, Aut
         super();
     }
 
-    /**
-     * @author hugeblank
-     * @reason Smooth out daylight cycle & remove client de-sync jitter.
-     */
-    @Shadow
-    @Overwrite
-    protected void tickTime() {
-        remainder += factor; // add remainder to factor
-        long increment = (long) remainder; // truncate floating value
-        this.setTime(this.getTime() + increment);
-        if (this.getGameRules().getBoolean(GameRules.getKeys().get("doDaylightCycle").toString())) {
-            this.setTimeOfDay(this.getTimeOfDay() + increment);
+    @Inject(at=@At("HEAD"), method = "tickTime", cancellable = true)
+    public void tickTime(CallbackInfo ci) {
+        if (this.isClient) {
+            remainder += factor; // add remainder to factor
+            long increment = (long) remainder; // truncate floating value
+            this.setTime(this.getTime() + increment);
+            if (shouldTickDay) this.setTimeOfDay(this.getTimeOfDay() + increment);
+            // subtract the incremented integer, preserving the floating point remainder for later
+            remainder -= increment;
+            ci.cancel();
         }
-        // subtract the incremented integer, preserving the floating point remainder for later
-        remainder -= increment;
     }
 
     @Shadow public abstract void setTime(long l);
@@ -53,18 +53,26 @@ public abstract class ClientWorldMixin implements ExtendedBlockView, IWorld, Aut
 
     @Shadow public abstract long getTime();
 
-    @Shadow public abstract GameRules getGameRules();
-
     @Shadow public abstract long getTimeOfDay();
+
+    @Shadow @Final public boolean isClient;
 
     @Override
     public void asahi$updateTimes(WorldTimeUpdateS2CPacket packet) {
         final int TPS = 20;
-        long currentPacketTime = packet.getTimeOfDay();
-        int localDiff = (int) (currentPacketTime - this.getTimeOfDay());
-        if (Math.abs(localDiff) >= 60* TPS) { // SKIP_DURATION
+        long currentPacketTime = packet.getTime();
+        if (lastPacketTimeOfDay == packet.getTimeOfDay()) {
+            if (!shouldTickDay) {
+                this.setTimeOfDay(packet.getTimeOfDay());
+            }
+            shouldTickDay = false;
+        } else {
+            shouldTickDay = true;
+        }
+        int localDiff = (int) (currentPacketTime - this.getTime());
+        if (Math.abs(localDiff) >= 60 * TPS) { // SKIP_DURATION
             this.setTime(packet.getTime());
-            this.setTimeOfDay(packet.getTimeOfDay());
+            if (shouldTickDay) this.setTimeOfDay(packet.getTimeOfDay());
         } else {
             float minMoveFactor = 1f/ TPS; // MIN_MOVE_FACTOR
             points.add((double) (localDiff + TPS) / TPS);
@@ -77,9 +85,10 @@ public abstract class ClientWorldMixin implements ExtendedBlockView, IWorld, Aut
                 avg += points.get(i)*weight;
             }
             avg /= weights;
-            // TODO: Debug logging that doesn't show up in prod
-            // System.out.println((localDiff < 0 ? "ahead of" : "behind") + " server by " + Math.abs(localDiff) + " ticks. Speed: " + avg);
+            if (FabricLoader.getInstance().isDevelopmentEnvironment())
+                System.out.println((localDiff < 0 ? "ahead of" : "behind") + " server by " + Math.abs(localDiff) + " ticks. Speed: " + avg);
             factor = avg < 0 ? Math.min(avg, -minMoveFactor) : Math.max(avg, minMoveFactor);
+            lastPacketTimeOfDay = packet.getTimeOfDay();
         }
     }
 }
